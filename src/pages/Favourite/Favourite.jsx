417c200f5c93721d3a8e33da066a7c2b2856c890
Kareem Azam <117589101+Karmo7X@users.noreply.github.com>
2023-04-14 00:17:24 +0200

    Update Favourite.jsx

--- src/pages/Favourite/Favourite.jsx
+++ src/pages/Favourite/Favourite.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import "./favourite.css"
+import Sidemenu from '../../component/sidemenu/Sidemenu'
 const Favourite = () => {
   return (
-    <div>
-            <h1 className="text-white">Favourite page</h1>
+   <div className="fav">
+    <Sidemenu/>
+    <div className="fav-contianer">
+      <h1 className="text-white">Favourite page</h1>
     </div>
+  </div>
   )
 }
 
