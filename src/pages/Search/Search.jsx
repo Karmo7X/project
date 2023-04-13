1ba4809e3b923169996dc508f797ffe5ed475c70
Kareem Azam <117589101+Karmo7X@users.noreply.github.com>
2023-04-14 00:10:54 +0200

    Update Search.jsx

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import Sidemenu from '../../component/sidemenu/Sidemenu'
+import "./search.css"
 function Search() {
   return (
-    <div>
-            <h1 className="text-white">Search page</h1>
+   <div className="search">
+      <Sidemenu/>
+      <div className="search-contianer">
+        <h1 className="text-white">search page</h1>
+      </div>
     </div>
   )
 }
